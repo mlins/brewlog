865bc50374a403dd04c48ecef936ee8972fe61e9
Matt Lins <mattlins@gmail.com>
2013-01-29 11:45:14 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewlog::Application.config.secret_token = 'eb69bfd61faec85e980345af31123191bfe0c68641428fff7f439d86c2430c5248063657fe59dd436b47c6c3b5d6bf72215c7c5c0beddb31af469c225999aad3'
